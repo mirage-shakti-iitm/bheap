a069888830e3cc7f64844be6d9892dc3ee93a5a6
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-07-08 08:25:32 +0530

    Added compartment allocation

---
+++ compartment_allocations/default_cap_files/binary_heap_cap_tee.ml
@@ -0,0 +1,15 @@
+camlBinary_heap__fun:254
+camlBinary_heap__Make:254
+camlBinary_heap__create:254
+camlBinary_heap__is_empty:254
+camlBinary_heap__resize:254
+camlBinary_heap__add:254
+camlBinary_heap__moveup:254
+camlBinary_heap__maximum:254
+camlBinary_heap__remove:254
+camlBinary_heap__movedown:254
+camlBinary_heap__pop_maximum:254
+camlBinary_heap__iter:254
+camlBinary_heap__fold:254
+camlBinary_heap__foldrec:254
+camlBinary_heap_:254
